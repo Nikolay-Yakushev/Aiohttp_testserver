4966217b1eab45056141aa5a35d94579b9966448
driver220v <driver220v@gmail.com>
2020-05-01 10:43:51 +0300

    upload_folder extraction changed

--- server/app/app_logic.py
+++ server/app/app_logic.py
@@ -1,41 +1,33 @@
 import os
-
 import aiofiles
 from aiohttp import web
 from werkzeug.utils import secure_filename
 
-from . import upload_folder
-
 
 async def _size(request):
+    upload_folder = request.app['config'].get('upload_folder')
     file_searched = request.rel_url.query['file']
     try:
-
         path_to_file = os.path.join(upload_folder, file_searched)
-        file_size = 0
-        async with aiofiles.open(path_to_file, 'rb') as outfile:
-            while True:
-                chunk = await outfile.read(8192)
-                if not chunk:
-                    break
-                file_size += len(chunk)
-        return web.Response(text=f'{file_searched} size is {file_size} bytes\n\n')
+        file_size = os.path.getsize(path_to_file)
+        return web.Response(text=f' {file_searched} size is {file_size} bytes\n\n ')
 
     except Exception as e:
         print(e)
 
 
 async def _upload(request):
+    upload_folder = request.app['config'].get('upload_folder')
     reader = await request.multipart()
     field = await reader.next()
     file_searched = field.name
     if file_searched == 'file':
         filename = secure_filename(field.filename)
-        response = await write_file(filename, field)
+        response = await write_file(upload_folder, filename, field)
         return response
 
 
-async def write_file(filename, field):
+async def write_file(upload_folder, filename, field):
     async with aiofiles.open(os.path.join(upload_folder, filename), 'wb') as infile:
         while True:
             chunk = await field.read_chunk(8192)
@@ -43,10 +35,11 @@ async def write_file(filename, field):
                 break
             await infile.write(chunk)
 
-    return web.Response(text=f'{filename} has been successfully stored\n\n')
+    return web.Response(text=f'{filename} has been successfully stored\n')
 
 
 async def _download(request):
+    upload_folder = request.app['config'].get('upload_folder')
     file_searched = request.rel_url.query['file']
     try:
         async with aiofiles.open(os.path.join(upload_folder, file_searched), mode='r') as outfile:
@@ -55,4 +48,4 @@ async def _download(request):
 
 
     except Exception as e:
-        web.Response(text='file not found')
+        return web.Response(text='file not found')
