be39350f6b72313b03864d474eb39b2c343a3e6b
driver220v <driver220v@gmail.com>
2020-05-01 10:42:59 +0300

    config initialisation changed

---
+++ server/app/app_init.py
@@ -0,0 +1,16 @@
+import yaml
+from aiohttp import web
+
+
+def create_app(path: str):
+    """Create an application."""
+    app = web.Application()
+
+    from .app_routes import routes
+    # from . import upload_folder
+    with open(path, 'r') as conf_file:
+        config = yaml.safe_load(conf_file)
+        app['config'] = config
+
+    app.add_routes(routes)
+    return app
